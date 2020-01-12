4d540171129ee387d6a36fb400fcb204f8e05a17
Ammar <ammarwm@gmail.com>
2020-01-13 10:06:09 +1100

    deleted src

--- src/sagify/local_test/deploy_local.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-test_path=$1
-tag=$2
-image=$3
-
-docker run -it -v ${test_path}:/opt/ml -p 8080:8080 --rm "${image}:${tag}" serve
--- src/sagify/local_test/train_local.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-test_path=$1
-tag=$2
-image=$3
-
-docker run -v ${test_path}:/opt/ml --rm "${image}:${tag}" train
